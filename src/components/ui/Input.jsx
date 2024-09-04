387797c2b4c46fe5bb9f48c7e7b4281eef9ff7c9
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-09-03 22:59:30 -0300

    refactor: troca do onchange do componente para garantir melhor personalização

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,6 +1,6 @@
 import { Eye } from "lucide-react";
 import { EyeOff } from "lucide-react";
-const Input = ({ label, value, setValue, id, type, children, seePassword, setSeePassword }) => {
+const Input = ({ label, value, setValue, id, type, children, seePassword, setSeePassword, }) => {
   if (type !== "password") {
     return (
       <div className="flex flex-col">
@@ -9,7 +9,7 @@ const Input = ({ label, value, setValue, id, type, children, seePassword, setSee
           type={type}
           id={id}
           value={value}
-          onChange={({ target }) => setValue(target.value)}
+          onChange={(e)=> setValue(e)}
           className="p-[1rem] text-[1.8rem] outline-none rounded-md bg-dark-300 bg-opacity-30 dark:bg-dark-800 dark:bg-opacity-80 dark:text-dark-200 ease-in-out duration-300 focus:dark:border-dark-500 hover:dark:border-dark-500 focus:border-dark-300 hover:border-dark-300 border border-transparent"
         />
         {children}
