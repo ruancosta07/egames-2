0fe20aa1729061e7b1bdbe170686a02b36389ca5
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-09-06 21:31:28 -0300

    .

--- src/views/privates/Seguranca.jsx
+++ src/views/privates/Seguranca.jsx
@@ -11,6 +11,8 @@ import useUserStore from "@store/UserStore";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Message from "@utils/Message";
+import { Switch } from "@components/ui/switch";
+import { Label } from "@components/ui/label";
 const Seguranca = () => {
   const {setUserActive, setSigned ,setCart, setFavorites, preferences, loadingData} = useUserStore()
   const [verificacaoDuasEtapas, setVerificacaoDuasEtapas] = useState(()=> {
@@ -30,7 +32,7 @@ const Seguranca = () => {
   async function confirmarSenha(){
     setLoading(true)
     try{
-      const response = await axios.post(`${import.meta.env.VITE_API_DEVELOPMENT}/conta/confirmar-senha`, {password}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})
+      const response = await axios.post(`${import.meta.env.VITE_API_PRODUCTION}/conta/confirmar-senha`, {password}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})
       setSenhaConfirmada(true)
     }
     catch(err){
@@ -39,11 +41,10 @@ const Seguranca = () => {
       setLoading(false)
     }
   }
-  console.log(preferences)
   async function mudarSenha(){
     setLoading(true)
     try{
-      const response = await axios.post(`${import.meta.env.VITE_API_DEVELOPMENT}/conta/alterar-senha`, {password:newPassword}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})
+      const response = await axios.post(`${import.meta.env.VITE_API_PRODUCTION}/conta/alterar-senha`, {password:newPassword}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})
       setMessage({type: "success", title: "Senha alterada com sucesso", text: "Por motivos de segurança, você precisa fazer login novamente"})
       console.log(response)
       clearTimeout(timeOut.current)
@@ -67,9 +68,9 @@ const Seguranca = () => {
   async function changeTwoStepsAuth(){
     const localstate = !verificacaoDuasEtapas
     setLoading(true)
-    // setVerificacaoDuasEtapas(!localstate)
+    setVerificacaoDuasEtapas(!verificacaoDuasEtapas)
     try{
-      const response = await axios.patch(`${import.meta.env.VITE_API_DEVELOPMENT}/conta/atualizar`, {preferences: {twoStepsAuth: localstate}}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})
+      const response = await axios.patch(`${import.meta.env.VITE_API_PRODUCTION}/conta/atualizar`, {preferences: {twoStepsAuth: localstate}}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})
     }
     catch(err){
       console.log(err)
@@ -78,89 +79,100 @@ const Seguranca = () => {
     }
   }
 
-
   return (
     <>
-    
-    <form className="max-w-[60%] mb-[8rem]">
-      <h1 className="dark:text-dark-50 text-[3rem] font-semibold mb-[2rem]">
-        Segurança da conta
-      </h1>
+      <form className="lg:max-w-[60%] mb-[8rem]">
+        <h1 className="dark:text-dark-50 text-[3rem] font-semibold mb-[2rem]">
+          Segurança da conta
+        </h1>
 
-      <div className="flex gap-[1rem]">
-        <label
-          htmlFor="verificacao-duas-etapas"
-          className={`w-[5.6rem] h-[3rem] ${
-            verificacaoDuasEtapas
-              ? "dark:bg-dark-100 bg-dark-800"
-              : "dark:bg-dark-800 bg-dark-300"
-          } rounded-[2rem] flex items-center relative`}
-        >
-          <div
-            className={`w-9 h-9 rounded-full absolute transition-transform duration-300 ease-in-out transform ${
-              verificacaoDuasEtapas
-                ? "dark:bg-dark-900 translate-x-[130%] bg-dark-50"
-                : "translate-x-[10%] bg-dark-600 dark:bg-dark-900"
-            }`}
-          ></div>
-          <input
-            type="checkbox"
-            className="opacity-0"
+        <div className="flex gap-[1rem]">
+          <Switch
             checked={verificacaoDuasEtapas}
-            onChange={() => {
-              setVerificacaoDuasEtapas((p) => !p)
-              changeTwoStepsAuth()
-            }}
-            name=""
-            id="verificacao-duas-etapas"
+            onCheckedChange={changeTwoStepsAuth}
+            id="two-steps-auth"
           />
-        </label>
-        <div>
-          <label
-            htmlFor="verificacao-duas-etapas"
-            className="dark:text-dark-100 text-[2rem] bricolage mb-[.4rem] block"
-          >
-            Verificação em duas etapas
-          </label>
-          <p className="dark:text-dark-300 text-[1.6rem]">
-            Receba um email com um código sempre que você for fazer login
-          </p>
+          <div>
+            <Label
+              htmlFor="two-steps-auth"
+              className="dark:text-dark-100 text-[2rem] bricolage mb-[.4rem] block"
+            >
+              Verificação em duas etapas
+            </Label>
+            <p className="dark:text-dark-300 text-[1.6rem]">
+              Receba um email com um código sempre que você for fazer login
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="mt-[4rem]">
-        <button
-          type="button"
-          onClick={() => setAlterarSenha(!alterarSenha)}
-          className="bg-dark-900 text-dark-100 dark:bg-zinc-200 dark:text-zinc-800 p-[1rem] text-[1.6rem] rounded-md font-medium"
+        <div className="mt-[4rem]">
+          <button
+            type="button"
+            onClick={() => setAlterarSenha(!alterarSenha)}
+            className="bg-dark-900 text-dark-100 dark:bg-zinc-200 dark:text-zinc-800 p-[1rem] text-[1.6rem] rounded-md font-medium"
+          >
+            Alterar senha
+          </button>
+        </div>
+      </form>
+      {alterarSenha && (
+        <Modal
+          type="form"
+          action={senhaConfirmada ? mudarSenha : confirmarSenha}
+          modal={alterarSenha}
+          setModal={setAlterarSenha}
         >
-          Alterar senha
-        </button>
-      </div>
-
-    </form>
-    {alterarSenha && (
-        <Modal type="form" action={senhaConfirmada ? mudarSenha : confirmarSenha} modal={alterarSenha} setModal={setAlterarSenha}>
-          {!senhaConfirmada && <>
-            <h1 className="text-[2.6rem] dark:text-dark-50 font-semibold mb-[2rem]">
-            Confirme sua identidade
-          </h1>
-            <Input value={password} setValue={setPassword} type={"password"} label={"Digite sua senha"} id={"confirmar-senha"} seePassword={verSenha} setSeePassword={setVerSenha}></Input>
-          {/* </div> */}
-          <button onClick={confirmarSenha} className="bg-dark-900 text-dark-100 dark:bg-zinc-50 dark:text-zinc-800 p-[1rem] mt-[1.2rem] text-[1.6rem] rounded-md font-medium">Confirmar senha</button>
-          </>}
-          {senhaConfirmada && <>
-            <h1 className="text-[2.6rem] dark:text-dark-50 font-semibold mb-[2rem]">
-            Digite sua nova senha
-          </h1>
-            <Input value={newPassword} setValue={setNewPassword} type={"password"} label={"Senha"} id={"confirmar-senha"} seePassword={verSenha} setSeePassword={setVerSenha}></Input>
-          {/* </div> */}
-          <button type="submit" onClick={confirmarSenha} className="bg-dark-900 text-dark-100 dark:bg-zinc-50 dark:text-zinc-800 p-[1rem] mt-[1.2rem] text-[1.6rem] rounded-md font-medium">Alterar senha</button>
-          </>}
+          {!senhaConfirmada && (
+            <>
+              <h1 className="text-[2.6rem] dark:text-dark-50 font-semibold mb-[2rem]">
+                Confirme sua identidade
+              </h1>
+              <Input
+                value={password}
+                setValue={setPassword}
+                type={"password"}
+                label={"Digite sua senha"}
+                id={"confirmar-senha"}
+                seePassword={verSenha}
+                setSeePassword={setVerSenha}
+              ></Input>
+              {/* </div> */}
+              <button
+                onClick={confirmarSenha}
+                className="bg-dark-900 text-dark-100 dark:bg-zinc-50 dark:text-zinc-800 p-[1rem] mt-[1.2rem] text-[1.6rem] rounded-md font-medium"
+              >
+                Confirmar senha
+              </button>
+            </>
+          )}
+          {senhaConfirmada && (
+            <>
+              <h1 className="text-[2.6rem] dark:text-dark-50 font-semibold mb-[2rem]">
+                Digite sua nova senha
+              </h1>
+              <Input
+                value={newPassword}
+                setValue={setNewPassword}
+                type={"password"}
+                label={"Senha"}
+                id={"confirmar-senha"}
+                seePassword={verSenha}
+                setSeePassword={setVerSenha}
+              ></Input>
+              {/* </div> */}
+              <button
+                type="submit"
+                onClick={confirmarSenha}
+                className="bg-dark-900 text-dark-100 dark:bg-zinc-50 dark:text-zinc-800 p-[1rem] mt-[1.2rem] text-[1.6rem] rounded-md font-medium"
+              >
+                Alterar senha
+              </button>
+            </>
+          )}
         </Modal>
       )}
-      {loading && <Loader/>}
-      {message && <Message {...message} setMessage={setMessage}/>}
+      {loading && <Loader />}
+      {message && <Message {...message} setMessage={setMessage} />}
     </>
   );
 };
