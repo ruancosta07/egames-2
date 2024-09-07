eb5452fddc37182a83b4b0e7fc0f57ee46ed2fb6
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-09-06 21:23:43 -0300

    refactor: verificação se é do tipo form para retornar um form

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 
-const Modal = ({ type, action, modal, setModal, children }) => {
+const Modal = ({ type, action, modal, setModal, children, onCloseModal }) => {
   const modalDiv = useRef();
   useEffect(() => {
     gsap.fromTo(
@@ -19,27 +19,32 @@ const Modal = ({ type, action, modal, setModal, children }) => {
       scale: 0.9,
       opacity: 0,
       duration: 0.15,
-      onComplete: () => setModal(false),
+      onComplete: () => {
+        if(onCloseModal){
+          onCloseModal();
+        }
+        setModal(false);
+      },
     });
   }
 
   return (
     <div
       onClick={(e) => closeModal()}
-      className=" w-[100vw] h-[100vh] fixed left-0 top-0 flex items-center justify-center bg-dark-900 bg-opacity-30 "
+      className=" w-[100vw] h-[100vh] fixed left-0 top-0 flex items-center justify-center bg-dark-900 bg-opacity-30 z-[8]"
     >
       {type === "form" ? (
         <form
-        onSubmit={(e)=> {
-          e.preventDefault()
-          action()
-        }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            action();
+          }}
           ref={modalDiv}
           onClick={(e) => e.stopPropagation()}
-          className="modal min-w-[30%] min-h-[10%] p-[2rem] dark:bg-dark-900 rounded-[.5rem] border dark:border-dark-700 relative"
+          className="modal min-w-[30%] max-w-[40%] min-h-[10%] max-h-[60%] overflow-y-scroll p-[2rem] dark:bg-dark-900 rounded-[.5rem] border dark:border-dark-700 relative"
         >
           <button
-          type="button"
+            type="button"
             onClick={(e) => closeModal(e)}
             className="absolute top-4 right-4 dark:text-dark-700 hover:bg-dark-600 hover:bg-opacity-30 p-[.5rem] rounded-md duration-200 ease-in-out"
           >
@@ -51,7 +56,8 @@ const Modal = ({ type, action, modal, setModal, children }) => {
         <div
           ref={modalDiv}
           onClick={(e) => e.stopPropagation()}
-          className="modal min-w-[30%] min-h-[10%] p-[2rem] dark:bg-dark-900 rounded-[.5rem] border dark:border-dark-700 relative"
+          onSelect={(e)=> e.stopPropagation()}
+          className="modal min-w-[30%] max-h-[30%] p-[2rem] dark:bg-dark-900 rounded-[.5rem] border dark:border-dark-700 relative"
         >
           <button
             onClick={(e) => closeModal(e)}
