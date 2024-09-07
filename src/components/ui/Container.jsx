589da5d979159dde8f7d9862bed0d5fc2cf4b586
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-09-06 21:20:47 -0300

    styles: responsivo

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -8,8 +8,8 @@ import { NavLink } from 'react-router-dom'
 const Container = ({children}) => {
   return (
     <section>
-      <div className="container-width mt-[10rem] grid" style={{gridTemplateColumns: ".4fr 1fr"}}>
-        <aside className="flex flex-col gap-[1rem] sidebar-user">
+      <div className="container-width mt-[10rem] grid lg:grid-cols-[.4fr_1fr]">
+        <aside className="flex lg:flex-col gap-[1rem] sidebar-user max-lg:mb-[2rem]  overflow-x-auto">
           <NavLink
             to={"/conta/perfil"}
             className={` text-[2rem] font-medium flex gap-[.8rem] items-center`}
