e847f6da209ca37900eead843288aa2ae5c25488
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-09-06 21:29:48 -0300

    refactor: responsivo e troca da url da api

--- src/views/privates/Favoritos.jsx
+++ src/views/privates/Favoritos.jsx
@@ -13,9 +13,13 @@ import { useEffect } from "react";
 import gsap from "gsap";
 import Message from "@utils/Message";
 import { useState } from "react";
+import { useRef } from "react";
 const Favoritos = () => {
   const { favorites, signed, setFavorites, setCart } = useUserStore();
-  const [message, setMessage] = useState(null)
+  const [message, setMessage] = useState(null);
+  const [showResumo, setShowResumo] = useState(true);
+  const lastScrollY = useRef(0);
+
   const desconto = () => {
     if (signed) {
       const cartNumber = favorites?.map((i) => Number(i.oldPrice));
@@ -36,7 +40,7 @@ const Favoritos = () => {
         const response = (
           await axios.delete(
             `${
-              import.meta.env.VITE_API_DEVELOPMENT
+              import.meta.env.VITE_API_PRODUCTION
             }/conta/favoritos/remover/${id}`,
             { headers: { Authorization: `Bearer ${token}` } }
           )
@@ -48,24 +52,51 @@ const Favoritos = () => {
     }
   }
 
- async  function moveItemToCart(id){
-    try{
-      const response = (await axios.post(`${import.meta.env.VITE_API_DEVELOPMENT}/conta/mover-para-carrinho`, {id}, {headers: {Authorization: `Bearer ${Cookies.get("auth_token_user")}`}})).data
-      setFavorites(response.favorites)
-      setCart(response.cart)
-      setMessage({type: "success", title: "Item movido para o carrinho"})
-    }
-    catch(err){
-      console.log(err)
-      setMessage({type: "error", title: "Esse item já está no seu carrinho"})
+  async function moveItemToCart(id) {
+    try {
+      const response = (
+        await axios.post(
+          `${import.meta.env.VITE_API_PRODUCTION}/conta/mover-para-carrinho`,
+          { id },
+          {
+            headers: {
+              Authorization: `Bearer ${Cookies.get("auth_token_user")}`,
+            },
+          }
+        )
+      ).data;
+      setFavorites(response.favorites);
+      setCart(response.cart);
+      setMessage({ type: "success", title: "Item movido para o carrinho" });
+    } catch (err) {
+      console.log(err);
+      setMessage({ type: "error", title: "Esse item já está no seu carrinho" });
     }
   }
 
-  useEffect(()=>{
-    if(favorites.length == 0){
-      gsap.fromTo("[data-animate='favoritos']", {opacity: 0, y: 32}, {opacity: 1, y: 0, stagger: .1})
+  useEffect(() => {
+    if (favorites.length == 0) {
+      gsap.fromTo(
+        "[data-animate='favoritos']",
+        { opacity: 0, y: 32 },
+        { opacity: 1, y: 0, stagger: 0.1 }
+      );
     }
-  },[favorites])
+  }, [favorites]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY.current) {
+        setShowResumo(false); // Oculta ao rolar para baixo
+      } else {
+        setShowResumo(true); // Mostra ao rolar para cima
+      }
+      lastScrollY.current = window.scrollY; // Atualiza a posição do scroll
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); // Limpeza do evento
+  }, []);
 
   if (favorites)
     return (
@@ -74,38 +105,37 @@ const Favoritos = () => {
           <div className="container-width">
             {favorites.length > 0 && (
               <>
-                <h1 className="text-[6rem] font-semibold dark:text-dark-100">
+                <h1 className=" text-[3rem] lg:text-[6rem] font-semibold dark:text-dark-100">
                   Meus Favoritos
                 </h1>
-                <div
-                  style={{
-                    display: "grid",
-                    gridTemplateColumns: "1fr .4fr",
-                    gap: "4rem",
-                  }}
-                >
+                <div className="grid lg:grid-cols-[1fr_.4fr] gap-[4rem] min-h-[100vh]">
                   <div className="itens flex flex-col justify-start gap-[3rem] mt-[1.2rem]">
                     {favorites.map((item) => {
                       return (
                         <div key={item.id} className="flex gap-[2rem]">
                           <img
                             src={item.images[0]}
-                            className="max-w-[240px] min-w-[240px] max-h-[240px] min-h-[240px] rounded-[1rem] object-cover"
+                            className="w-[100px] h-[100px] lg:w-[240px] lg:h-[240px] rounded-[1rem] object-cover"
                             alt=""
                           />
                           <div>
-                            <span className="dark:text-dark-100 text-[3rem] font-semibold mb-[1rem] block">
+                            <span className="dark:text-dark-100 leading-[1.215] text-[2rem] lg:text-[3rem] font-semibold mb-[1rem] block">
                               {item.title}
                             </span>
                             <div className="flex gap-[1rem] items-center">
-                              <span className="dark:text-dark-100 text-[2.4rem] font-semibold">
+                              <span className="dark:text-dark-100 text-[1.8rem]  lg:text-[2.4rem] font-semibold">
                                 R$ {item.price}
                               </span>
-                              <span className="dark:text-dark-500 text-[1.8rem] font-semibold line-through italic">
+                              <span className="dark:text-dark-500 text-[1.4rem] lg:text-[1.8rem] font-semibold line-through italic">
                                 R$ {item.oldPrice}
                               </span>
                             </div>
-                            <button onClick={()=> moveItemToCart(item._id)} className="p-[1rem] dark:bg-dark-50 dark:text-dark-900 text-[1.7rem] mt-[1.2rem] rounded-[.5rem] font-medium">Mover para o carrinho</button>
+                            <button
+                              onClick={() => moveItemToCart(item._id)}
+                              className="p-[1rem] dark:bg-dark-50 dark:text-dark-900 text-[1.2rem] max-lg:w-full lg:text-[1.7rem] mt-[1.2rem] rounded-[.5rem] font-medium"
+                            >
+                              Mover para o carrinho
+                            </button>
                           </div>
                           <button
                             onClick={() => removeItemFromFavorites(item._id)}
@@ -117,52 +147,64 @@ const Favoritos = () => {
                       );
                     })}
                   </div>
-                  <div className="resumo sticky top-8 h-fit">
-                    <h3 className="dark:text-dark-100 text-[3rem] font-semibold mb-[.8rem]">
-                      Resumo do pedido
-                    </h3>
-                    <div className="flex justify-between mb-[1.2rem]">
-                      <span className="dark:text-dark-300 text-[1.75rem]">
-                        Total de itens
-                      </span>
-                      <span className="dark:text-dark-300 text-[1.75rem]">
-                        {favorites.length}
-                      </span>
+                  {showResumo && (
+                    <div className="resumo max-lg:fixed max-lg:bottom-0 lg:sticky lg:top-8 h-fit bg-dark-900 left-0 w-full p-[2rem] py-[3rem]">
+                      <h3 className="dark:text-dark-100 text-[2rem] lg:text-[3rem] font-semibold mb-[.8rem]">
+                        Resumo do pedido
+                      </h3>
+                      <div className="flex justify-between mb-[1.2rem]">
+                        <span className="dark:text-dark-300 text-[1.75rem]">
+                          Total de itens
+                        </span>
+                        <span className="dark:text-dark-300 text-[1.75rem]">
+                          {favorites.length}
+                        </span>
+                      </div>
+                      <div className="flex justify-between mb-[1.2rem]">
+                        <span className="dark:text-dark-300 text-[1.75rem]">
+                          Desconto
+                        </span>
+                        <span className="dark:text-dark-300 text-[1.75rem]">
+                          R$ {(desconto() - total()).toFixed(2)}
+                        </span>
+                      </div>
+                      <div className="flex justify-between">
+                        <span className="dark:text-dark-100 text-[2.4rem] font-semibold">
+                          Total
+                        </span>
+                        <span className="dark:text-dark-100 text-[2.4rem] font-semibold">
+                          R$ {total().toFixed(2)}
+                        </span>
+                      </div>
                     </div>
-                    <div className="flex justify-between mb-[1.2rem]">
-                      <span className="dark:text-dark-300 text-[1.75rem]">
-                        Desconto
-                      </span>
-                      <span className="dark:text-dark-300 text-[1.75rem]">
-                        R$ {(desconto() - total()).toFixed(2)}
-                      </span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="dark:text-dark-100 text-[2.4rem] font-semibold">
-                        Total
-                      </span>
-                      <span className="dark:text-dark-100 text-[2.4rem] font-semibold">
-                        R$ {total().toFixed(2)}
-                      </span>
-                    </div>
-                  </div>
+                  )}
                 </div>
               </>
             )}
 
             {favorites.length == 0 && (
               <>
-                <HeartCrack className="w-[12.8rem] h-[12.8rem] dark:text-dark-100 mx-auto mb-[2rem] mt-[15vh]" data-animate="favoritos" />
-                <h1 className="dark:text-dark-100 text-[8rem] font-semibold mx-auto text-center max-w-[20ch] mb-[1.2rem]" data-animate="favoritos">
+                <HeartCrack
+                  className="h-[8rem] w-[8rem] lg:w-[12.8rem] lg:h-[12.8rem] dark:text-dark-100 mx-auto mb-[1.2rem] lg:mb-[2rem] mt-[10vh] lg:mt-[15vh]"
+                  data-animate="favoritos"
+                />
+                <h1
+                  className="dark:text-dark-100 text-[2.6rem] leading-[1.215] lg:text-[8rem] font-semibold mx-auto text-center max-w-[20ch] mb-[.4rem] lg:mb-[1.2rem]"
+                  data-animate="favoritos"
+                >
                   Oops, parece que sua lista de favoritos está vazia
                 </h1>
-                <p className="text-dark-300 text-[2rem] text-center" data-animate="favoritos">
+                <p
+                  className="text-dark-300 text-[1.4rem] lg:text-[2rem] text-center"
+                  data-animate="favoritos"
+                >
                   Assim que você adicionar itens aos seus favoritos eles irão
                   aparecer aqui.
                 </p>
                 <Link
                   to={"/"}
-                  className="mx-auto block dark:bg-dark-100 dark:text-dark-900 w-fit mt-[2rem] p-[1rem] font-semibold text-[2rem] rounded-md" data-animate="favoritos"
+                  className="mx-auto block dark:bg-dark-100 dark:text-dark-900 w-fit mt-[2rem] p-[1rem] font-semibold  text-[1.6rem] lg:text-[2rem] rounded-md"
+                  data-animate="favoritos"
                 >
                   Adicionar itens aos favoritos
                 </Link>
